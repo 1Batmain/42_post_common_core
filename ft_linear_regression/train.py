1628e17664507858e06f51dba55cc015280eace9
1Batmain <batduval@gmail.com>
2026-02-02 16:11:37 +0100

    sisi

--- ft_linear_regression/train.py
+++ ft_linear_regression/train.py
@@ -3,19 +3,24 @@ from utils import estimate_price
 import matplotlib.pyplot as plt
 
 
-def parse_data():
-    with open('data.csv', mode='r') as file:
-        csv_reader = csv.reader(file)
-        mileages = []
-        prices = []
-        next(csv_reader)  # Skip header
-        for row in csv_reader:
-            mileages.append(float(row[0]))
-            prices.append(float(row[1]))
+# def parse_data():
+#     with open('data.csv', mode='r') as file:
+#         csv_reader = csv.reader(file)
+#         mileages = []
+#         prices = []
+#         next(csv_reader)  # Skip header
+#         for row in csv_reader:
+#             mileages.append(float(row[0]))
+#             prices.append(float(row[1]))
+#     return mileages, prices
+
+def parse_data ():
+    mileages = [0, 1 , 2, 3, 5 , 28]
+    prices = [ 10, 12, 15, 17, 20, 30]
     return mileages, prices
 
 
-def train(mileages, prices, line, learning_rate=0.001, epochs=90):
+def train(mileages, prices, learning_rate=0.001, epochs=500):
     teta0 = 1
     teta1 = 1
 
@@ -24,41 +29,28 @@ def train(mileages, prices, line, learning_rate=0.001, epochs=90):
         tmpTeta1 = learning_rate * sum((estimate_price(mileages[i], teta0, teta1) - prices[i]) * mileages[i] for i in range(len(mileages))) / len(mileages)
         teta0 = teta0 - tmpTeta0
         teta1 = teta1 - tmpTeta1
-        if not epoch % 10:
-            update_plot(mileages, prices, teta0, teta1, line, 0)
-    update_plot(mileages, prices, teta0, teta1, line, 1)
 
 
     return teta0, teta1
 
-def init_plot_results(mileages, prices, teta0 = 0, teta1 = 0):
-    plt.ion()
+def plot_results(mileages, prices, teta0 = 0, teta1 = 0):
     plt.scatter(mileages, prices, color='blue', label='Data Points')
     line_x = [min(mileages), max(mileages)]
     line_y = [estimate_price(x, teta0, teta1) for x in line_x]
-    line, = plt.plot(line_x, line_y, color='red', label='Regression Line')
+    plt.plot(line_x, line_y, color='red', label='Regression Line')
     plt.xlabel('Mileage')
     plt.ylabel('Price')
     plt.title('Linear Regression Result')
     plt.legend()
-    return line
+    plt.show()
 
-def update_plot(mileages, prices, teta0, teta1, line, last=0):
-    line_x = [min(mileages), max(mileages)]
-    line_y = [estimate_price(x, teta0, teta1) for x in line_x]
-    line.set_ydata(line_y)
-    plt.draw()
-    plt.pause(1)
-    if last == 1:
-        plt.ioff()
-        plt.show()
 
 
 def main():
     print("Training started...")
     mileages, prices = parse_data()
-    line = init_plot_results(mileages, prices)
-    teta0, teta1 = train(mileages, prices, line)
+    teta0, teta1 = train(mileages, prices)
+    plot_results(mileages, prices, teta0, teta1)
     print(f"Training completed. teta0: {teta0}, teta1: {teta1}")
 
 if __name__ == "__main__":
