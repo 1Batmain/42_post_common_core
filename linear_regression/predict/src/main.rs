8450bbf0de7d6b399f9d31df76d36cffa70f0b0e
1Batmain <batduval@gmail.com>
2026-03-20 09:20:28 +0100

    add colored dependencie to train & correct unused var warning

--- linear_regression/predict/src/main.rs
+++ linear_regression/predict/src/main.rs
@@ -1,6 +1,6 @@
+use shared_lib::{Model, predict};
 use std::env;
 use std::error::Error;
-use shared_lib::{predict, Model};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -10,20 +10,37 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     let path = "data/model.json";
-    
+
     let serialized = match std::fs::read_to_string(path) {
-        Ok(content) => { println!("Getting weights from {path}"); content },
-        Err(e) => {eprintln!("Failed to read the file : {}", e); std::process::exit(1)},
+        Ok(content) => {
+            println!("Getting weights from {path}");
+            content
+        }
+        Err(e) => {
+            eprintln!("Failed to read the file : {}", e);
+            std::process::exit(1)
+        }
     };
     let model: Model = match serde_json::from_str(&serialized) {
-        Ok(model) => {println!("Model weights sets !"); model },
-        Err(e) => {eprintln!("Failed to parse model weights, try to run the train program again : {}", e); std::process::exit(1) },
+        Ok(model) => {
+            println!("Model weights sets !");
+            model
+        }
+        Err(e) => {
+            eprintln!(
+                "Failed to parse model weights, try to run the train program again : {}",
+                e
+            );
+            std::process::exit(1)
+        }
     };
-
 
     let mileage: u64 = match args[1].parse() {
         Ok(mileage) => mileage,
-        Err(e) => { eprintln!("cant estimate price for \"{}\" because itsm", args[1]); std::process::exit(1) },
+        Err(_e) => {
+            eprintln!("cant estimate price for \"{}\" because itsm", args[1]);
+            std::process::exit(1)
+        }
     };
 
     let price = predict(mileage, &model) as u64;
--- linear_regression/train/src/main.rs
+++ linear_regression/train/src/main.rs
@@ -1,25 +1,17 @@
-use std::error::Error;
+use colored::Colorize;
 use plotters::prelude::*;
 use plotters::series::LineSeries;
 use plotters::style::full_palette::{BLUE, RED};
-use shared_lib::{
-    Field,
-    Model,
-    normalize,
-    parse,
-    predict,
-};
+use shared_lib::{Field, Model, normalize, parse, predict};
+use std::error::Error;
 
-
-fn mean_km(data: &[Field]) -> f64
-{
+fn mean_km(data: &[Field]) -> f64 {
     let sum: f64 = data.iter().map(|field| field.km as f64).sum();
     let count = data.len() as f64;
     sum / count
 }
 
-fn standard_deviation(mean: f64, data: &[Field]) -> f64
-{
+fn standard_deviation(mean: f64, data: &[Field]) -> f64 {
     let variance: f64 = data
         .iter()
         .map(|field| {
@@ -31,50 +23,70 @@ fn standard_deviation(mean: f64, data: &[Field]) -> f64
     variance.sqrt()
 }
 
-fn get_data_parameters(data: &[Field]) -> Model
-{
+fn get_data_parameters(data: &[Field]) -> Model {
     let mean = mean_km(data);
     let standard_deviation = standard_deviation(mean, data);
-    let (theta0, theta1) = (0.,0.);
-    Model {theta0, theta1, mean, standard_deviation}
+    let (theta0, theta1) = (0., 0.);
+    Model {
+        theta0,
+        theta1,
+        mean,
+        standard_deviation,
+    }
 }
 
-fn train(data: &[Field]) -> Model
-{
+fn train(data: &[Field]) -> Model {
     let mut model = get_data_parameters(data);
     let epochs = 50000;
     let learning_rate = 0.001;
-    
 
-    for _epoch in 0..epochs
-    {
-        let theta0_tmp = model.theta0 - learning_rate * data.iter().map(|f| {
-            predict(f.km, &model) - f.price 
-        }).sum::<f64>() / data.len() as f64;
-        let theta1_tmp = model.theta1 - learning_rate * data.iter().map(|f| {
-            (predict(f.km, &model) - f.price) * normalize(f.km, &model)
-        }).sum::<f64>() / data.len() as f64;
+    for _epoch in 0..epochs {
+        let theta0_tmp = model.theta0
+            - learning_rate
+                * data
+                    .iter()
+                    .map(|f| predict(f.km, &model) - f.price)
+                    .sum::<f64>()
+                / data.len() as f64;
+        let theta1_tmp = model.theta1
+            - learning_rate
+                * data
+                    .iter()
+                    .map(|f| (predict(f.km, &model) - f.price) * normalize(f.km, &model))
+                    .sum::<f64>()
+                / data.len() as f64;
         model.theta0 = theta0_tmp;
         model.theta1 = theta1_tmp;
     }
     model
 }
 
-fn draw(data: &[Field], model: &Model, path : &str) -> Result<(), Box<dyn Error>>
-{
+fn draw(data: &[Field], model: &Model, path: &str) -> Result<(), Box<dyn Error>> {
     let root = BitMapBackend::new(path, (640, 480)).into_drawing_area();
     root.fill(&WHITE)?;
-    
-    let min_km = data.iter().map(|f| f.km as f64).fold(f64::INFINITY, |a, b| a.min(b));
-    let max_km = data.iter().map(|f| f.km as f64).fold(f64::NEG_INFINITY, |a, b| a.max(b));
-    let min_price = data.iter().map(|f| f.price).fold(f64::INFINITY, |a, b| a.min(b));
-    let max_price = data.iter().map(|f| f.price).fold(f64::NEG_INFINITY, |a, b| a.max(b));
-    
+
+    let min_km = data
+        .iter()
+        .map(|f| f.km as f64)
+        .fold(f64::INFINITY, |a, b| a.min(b));
+    let max_km = data
+        .iter()
+        .map(|f| f.km as f64)
+        .fold(f64::NEG_INFINITY, |a, b| a.max(b));
+    let min_price = data
+        .iter()
+        .map(|f| f.price)
+        .fold(f64::INFINITY, |a, b| a.min(b));
+    let max_price = data
+        .iter()
+        .map(|f| f.price)
+        .fold(f64::NEG_INFINITY, |a, b| a.max(b));
+
     let km_min = min_km * 0.9;
     let km_max = max_km * 1.1;
     let price_min = min_price * 0.9;
     let price_max = max_price * 1.1;
-    
+
     let mut chart = ChartBuilder::on(&root)
         .caption("Linear Regression", ("sans-serif", 50).into_font())
         .margin(20)
@@ -90,14 +102,17 @@ fn draw(data: &[Field], model: &Model, path : &str) -> Result<(), Box<dyn Error>
         Circle::new((x, y), 5, BLUE.filled())
     }))?;
 
-    let line_points: Vec<(f64, f64)> = (0..=100).map(|i| {
-        let x = km_min + (km_max - km_min) * (i as f64 / 100.0);
-        (x, predict(x as u64, model))
-    }).collect();
-    
-    chart.draw_series(LineSeries::new(line_points, RED))?
-    .label("Regression Line")
-    .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], RED));
+    let line_points: Vec<(f64, f64)> = (0..=100)
+        .map(|i| {
+            let x = km_min + (km_max - km_min) * (i as f64 / 100.0);
+            (x, predict(x as u64, model))
+        })
+        .collect();
+
+    chart
+        .draw_series(LineSeries::new(line_points, RED))?
+        .label("Regression Line")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], RED));
 
     chart
         .configure_series_labels()
@@ -110,30 +125,40 @@ fn draw(data: &[Field], model: &Model, path : &str) -> Result<(), Box<dyn Error>
     Ok(())
 }
 
-
-fn main() -> Result<(), Box<dyn Error>>
-{
+fn main() -> Result<(), Box<dyn Error>> {
     let path = "data/data.csv";
-    let data = match parse(path){
-        Ok(data) => {println!("Getting data from {}", path); data},
-        Err(e) => {println!("Fail to get data from {}:{}", path, e); std::process::exit(1);}
+    let data = match parse(path) {
+        Ok(data) => {
+            println!("Getting data from {}", path);
+            data
+        }
+        Err(e) => {
+            println!("Fail to get data from {}:{}", path, e);
+            std::process::exit(1);
+        }
     };
-    
+
     let model = train(&data);
     let serialize = match serde_json::to_string(&model) {
         Ok(serialize) => serialize,
         Err(e) => {
             println!("Fail to serialize model data :{}", e);
-            std::process::exit(1); 
-        },
+            std::process::exit(1);
+        }
     };
-    match std::fs::write("data/model.json", serialize){
-        Ok(_a) => println!("Model successfully trained and saved in {}", path.bold().green()),
+    match std::fs::write("data/model.json", serialize) {
+        Ok(_a) => println!(
+            "Model successfully trained and saved in {}",
+            path.bold().green()
+        ),
         Err(e) => println!("Fail to save the model in {}: {}", path.red(), e),
     };
     let graph_path = "data/model.png";
     match draw(&data, &model, graph_path) {
-        Ok(_s) => println!("Graph of linear regression saved in {}", graph_path.bold().green()),
+        Ok(_s) => println!(
+            "Graph of linear regression saved in {}",
+            graph_path.bold().green()
+        ),
         Err(_e) => println!("Fail to save the graph in {}", graph_path.red()),
     };
     Ok(())
